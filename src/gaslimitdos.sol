9865765d8450b589424649a3e41418821375e2c6
MV <m.viey@wanadoo.fr>
2024-01-13 20:41:00 +0100

    clean

--- src/gaslimitdos.sol
+++ src/gaslimitdos.sol
@@ -11,6 +11,7 @@ contract Voting is Ownable {
         bool hasVoted;
         uint votedProposalId;
     }
+    
     struct Proposal {
         string description;
         uint voteCount;
@@ -23,7 +24,7 @@ contract Voting is Ownable {
         VotingSessionEnded,
         VotesTallied
     }
-
+    
     WorkflowStatus public workflowStatus;
     uint private winningProposalId;
 
@@ -32,48 +33,72 @@ contract Voting is Ownable {
     Proposal[] public proposals;
 
     event VoterRegistered(address voterAddress);
-    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    event WorkflowStatusChange(WorkflowStatus previousStatus,
+                               WorkflowStatus newStatus);
     event ProposalRegistered(uint proposalId);
     event Voted (address voter, uint proposalId);
 
     modifier flowStatus(WorkflowStatus _status) {
-        require(workflowStatus == _status, "You are not able to do this action right now");
+        require(workflowStatus == _status,
+                "You are not able to do this action right now");
         _;
     }
 
     constructor() Ownable(msg.sender){ }
 
-    function registerVoters(address _voterAddress) public flowStatus(WorkflowStatus.RegisteringVoters) onlyOwner {
-        require(!voters[_voterAddress].isRegistered, "This address is already in voters");
+    function registerVoters(address _voterAddress)
+            public
+            flowStatus(WorkflowStatus.RegisteringVoters)
+            onlyOwner {
+        require(!voters[_voterAddress].isRegistered,
+                "This address is already in voters");
 
         voters[_voterAddress].isRegistered = true;
 
         emit VoterRegistered(_voterAddress);
     }
 
-    function startProposalsRegistration() public flowStatus(WorkflowStatus.RegisteringVoters) onlyOwner {
+    function startProposalsRegistration()
+            public
+            flowStatus(WorkflowStatus.RegisteringVoters)
+            onlyOwner {
         workflowStatus = WorkflowStatus.ProposalsRegistrationStarted;
-        emit WorkflowStatusChange(WorkflowStatus.RegisteringVoters, WorkflowStatus.ProposalsRegistrationStarted);
+        emit WorkflowStatusChange(WorkflowStatus.RegisteringVoters,
+                                  WorkflowStatus.ProposalsRegistrationStarted);
     }
 
-    function registerProposals(string memory proposalDescription) public flowStatus(WorkflowStatus.ProposalsRegistrationStarted) {
-        require(voters[msg.sender].isRegistered, "You are not allowed to make a proposal");
+    function registerProposals(string memory proposalDescription)
+            public
+            flowStatus(WorkflowStatus.ProposalsRegistrationStarted) {
+        require(voters[msg.sender].isRegistered,
+                "You are not allowed to make a proposal");
 
         proposals.push(Proposal(proposalDescription, 0));
         voters[msg.sender].votedProposalId = proposals.length + 1;
     }
    
-    function endProposalsRegistration() public flowStatus(WorkflowStatus.ProposalsRegistrationStarted) onlyOwner {
+    function endProposalsRegistration()
+            public
+            flowStatus(WorkflowStatus.ProposalsRegistrationStarted)
+            onlyOwner {
         workflowStatus = WorkflowStatus.ProposalsRegistrationEnded;
-        emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationStarted, WorkflowStatus.ProposalsRegistrationEnded);
+        emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationStarted,
+                                  WorkflowStatus.ProposalsRegistrationEnded);
     }
 
-    function startVotingSession() public flowStatus(WorkflowStatus.ProposalsRegistrationEnded) onlyOwner {
+    function startVotingSession()
+            public
+            flowStatus(WorkflowStatus.ProposalsRegistrationEnded)
+            onlyOwner {
         workflowStatus = WorkflowStatus.VotingSessionStarted;
-        emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationEnded, WorkflowStatus.VotingSessionStarted);
+        emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationEnded,
+                                  WorkflowStatus.VotingSessionStarted);
     }
 
-    function vote(uint proposalId) virtual public flowStatus(WorkflowStatus.VotingSessionStarted) {
+    function vote(uint proposalId)
+            virtual
+            public
+            flowStatus(WorkflowStatus.VotingSessionStarted) {
         require(voters[msg.sender].isRegistered, "You are not allowed to vote");
         require(!voters[msg.sender].hasVoted, "You have already voted");
         proposals[proposalId].voteCount += 1;
@@ -83,12 +108,19 @@ contract Voting is Ownable {
         emit Voted(msg.sender, proposalId);
     }
 
-    function endVotingSession() public flowStatus(WorkflowStatus.VotingSessionStarted) onlyOwner {
+    function endVotingSession()
+            public
+            flowStatus(WorkflowStatus.VotingSessionStarted)
+            onlyOwner {
         workflowStatus = WorkflowStatus.VotingSessionEnded;
-        emit WorkflowStatusChange(WorkflowStatus.VotingSessionStarted, WorkflowStatus.VotingSessionEnded);
+        emit WorkflowStatusChange(WorkflowStatus.VotingSessionStarted,
+                                  WorkflowStatus.VotingSessionEnded);
     }
 
-    function votesTallied() public flowStatus(WorkflowStatus.VotingSessionEnded) onlyOwner { 
+    function votesTallied()
+            public
+            flowStatus(WorkflowStatus.VotingSessionEnded)
+            onlyOwner { 
         uint winningVoteCount = 0;
         for (uint p = 0; p <proposals.length; p++) {
             if (proposals[p].voteCount > winningVoteCount) {
@@ -97,10 +129,15 @@ contract Voting is Ownable {
             }
         }
         workflowStatus = WorkflowStatus.VotesTallied;
-        emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.VotesTallied);
+        emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded,
+                                  WorkflowStatus.VotesTallied);
     }
 
-    function getWinner() public flowStatus(WorkflowStatus.VotesTallied) view returns (Proposal memory) {
+    function getWinner()
+            public
+            flowStatus(WorkflowStatus.VotesTallied)
+            view
+            returns (Proposal memory) {
         return proposals[winningProposalId];
     }
 }
